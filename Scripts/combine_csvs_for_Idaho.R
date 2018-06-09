478cfaa8ba6f2930b2e2648cb0f66dfc675d7361
Wolfson <12001003424299@FEDIDCARD.GOV>
2018-06-09 08:56:10 -0600

    finished

--- Scripts/combine_csvs_for_Idaho.R
+++ Scripts/combine_csvs_for_Idaho.R
@@ -4,14 +4,15 @@ library(tidyverse)
 # 1) go through and load all the csvs into one big file
 # 2) work through the mess of the csvs and get a species label
 
+#pull off column names for template to fill in loop
 df <- read_csv("E:/ID_test/site_A/AM1/Trip 1/100RECNX/TimelapseData.csv")
 my_names<-colnames(df)
 
 
 
-alldat<-data.frame(matrix(NA, nrow=1, ncol=58))
-colnames(alldat)<-my_names
-colnames(alldat)[58]<-'X58'
+alldat<-data.frame(matrix(NA, nrow=1, ncol=59))
+colnames(alldat)<-c(my_names, 'filename')
+
   #Task 1 should be a relatively simple looping exercise
 
 # I know it's slow and memory inefficient to grow objects during a loop, but I won't know the size of each csv to 
@@ -33,7 +34,7 @@ for (i in 1:length(level1_dirs)){
   setwd(tmp_path1)
   level2_dirs<-list.files(pattern='AM') #so that it won't get hung up on those two folders with csv's
   level2_head<-level1_dirs[i] 
-  print(i) #tells me when it's half done
+  print(level1_dirs[i]) #tells me when it's half done
   
   #second level
   for(j in 1:length(level2_dirs)){
@@ -41,7 +42,7 @@ for (i in 1:length(level1_dirs)){
     setwd(tmp_path2)
     level3_dirs<-list.files(pattern='Trip*')  #only grab folders named (trip 1, trip 2, etc), and not the backup folder
     level3_head<-paste(level2_head, level2_dirs[j], sep="_")
-    # would've been nice to add paste(i, j, sep='_) to see when each camera is done
+    print(paste(level1_dirs[i], level2_dirs[j], sep='_'))
     
     #third level  
     for(k in 1:length(level3_dirs)){
@@ -53,12 +54,16 @@ for (i in 1:length(level1_dirs)){
       #fourth level
       for(m in 1:length(level4_dirs)){
         tmp_path4<-paste(tmp_path3, level4_dirs[m], sep='/')
+        level5_head<-paste(level4_head, level4_dirs[m], sep='_')
         setwd(tmp_path4)
+        level5_files<-list.files(pattern='(.\\.jpg|\\.JPG)$') #only grab images 
         tmpdf<-read_csv('TimelapseData.csv')
         if(ncol(tmpdf)==57){
           tmpdf[,58]<-NA
           colnames(tmpdf)[58]<-'X58'
         }
+        tmpdf[,59]<-level5_files
+        colnames(tmpdf)[59]<-'filename'
         alldat<-rbind(alldat, tmpdf)
       }
     }
@@ -70,28 +75,43 @@ for (i in 1:length(level1_dirs)){
 # I fixed this issue by adding another column to the tibble if it needs it.
 
 
+#This isn't exactly what I want because I didn't grab the filename and insert it in while I was at it
+# but I'll write it out as an intermediate output to save
+setwd('C:/Users/apdwwolfson/Documents/Projects/Photo_Database/Machine_Learning/ML_file_prep')
+alldat<-alldat[-1,]
+write_csv(alldat, 'output/alldat.csv')
 
+#Trim some columns to save space
+names(alldat)
+df<-alldat[,c('filename', 'Date', 'Time', 'ImageQuality', 'MarkForDeletion', 
+              'elkpresent', 'deerpresent', 'prongpresent', 'humanpresent', 'otherpresent',
+              "ElkSpike", "ElkAntlerless",   "ElkCalf","MDbuck","MDantlerless","MDfawn",         
+              "WTDbuck","WTDantlerless","WTDfawn","MooseBull","MooseAntlerless", "MooseCalf","PronghornBuck" , 
+              "PronghornFawn","BlackBearAdult","BlackBearCub","LionAdult", "LionKitten","WolfAdult","WolfPup" ,       
+              "other" , "otherwhat","PHdoe","CattleCow","CattleCalf","ElkRaghorn","ElkMatBull","ElkUnkn",
+              "MooseUnkn", "PHunkn","MDunkn", "WTDunkn")]
+#save trimmed df
+write_csv(df, 'output/df.csv') #saves ~100MB, 182 vs 295
 
 
 
 
 # Task 2
-#after reading the csv, I should probably only keep the info I need and dump the rest
-df <- read.csv("E:/ID_test/site_A/AM1/Trip 1/100RECNX/TimelapseData.csv", stringsAsFactors=FALSE)
+#Assign species label
+df$species<-ifelse(df$elkpresent=='true' & df$prongpresent=='false' & df$deerpresent=='false' & df$humanpresent=='false', 'elk',
+        ifelse(df$prongpresent=='true' & df$elkpresent=='false' & df$deerpresent=='false' & df$humanpresent=='false', 'pronghorn',
+          ifelse(df$humanpresent=='true' & df$elkpresent=='false' & df$prongpresent=='false' & df$deerpresent=='false', 'human',
+            ifelse(df$MDantlerless>0 | df$MDbuck>0 | df$MDfawn>0 | df$MDunkn>0, 'mule deer',
+              ifelse(df$WTDantlerless>0 |df$WTDbuck>0 | df$WTDfawn>0 | df$WTDunkn>0, 'white-tailed deer',
+                ifelse(df$MooseAntlerless>0 | df$MooseBull>0 | df$MooseCalf>0 | df$MooseUnkn>0, 'moose',
+                  ifelse(df$BlackBearAdult>0 | df$BlackBearCub>0, 'black bear',
+                    ifelse(df$LionAdult>0 | df$LionKitten>0, 'mountain lion',
+                      ifelse(df$WolfAdult>0 | df$WolfPup>0, 'wolf',
+                        ifelse(df$other, df$otherwhat,     
+                          ifelse(df$CattleCalf>0 | df$CattleCow>0, 'cattle', 'flag')))))))))))
 
-
-
-
-
-
-
-# I can't use the 5 'species present columns because there 
-
-
-df$sp<-ifelse(df$elkpresent='true', 'elk',
-              ifelse(df$prongpresent='true', 'pronghorn',
-                     ifelse(df$h)))
-
+#overwrite with new species column
+write_csv(df, 'output/df.csv') 
 
 
 
@@ -101,6 +121,9 @@ df$sp<-ifelse(df$elkpresent='true', 'elk',
 
 
 ########################################################
+# I tested a few different options for assigning the species category
+# to see what would be fastest.
+
 library(microbenchmark)
 library(tidyverse)
 id <- c("a", "b", "c", "d")
@@ -163,4 +186,5 @@ mbm1
 # 176.682  195.0100  231.8604  219.0195  254.7595   447.569   100
 # 3499.543 3650.1985 4109.3181 3743.1220 3961.4075 30069.233   100
 # 609.222  642.7625  690.2282  673.0030  710.3925  1012.071   100
-# normal ifelse still wins
+
+# normal ifelse still wins; beats if_else and case_when
